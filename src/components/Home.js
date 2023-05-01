ba8fd4d33d43722586eb1c11311994f013630915
QUANDEVV <vercelploy@gmail.com>
2023-05-01 19:47:39 +0300

    Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-white pt-20'>Series</div>
+    <div className='text-white pt-20'>
+ 
+
+
+    <div className='pt-80'>
+      upcoming
+    </div>
+    <div>
+     Popular
+    </div>
+    <div>
+    Top Rated
+    </div><div>
+      upcoming
+    </div><div>
+      Horror
+    </div>
+    
+    </div>
   )
 }
 
