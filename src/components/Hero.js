aeb8dd865dd531dc95b4a61988c0cabf57348928
QUANDEVV <vercelploy@gmail.com>
2023-05-02 10:28:58 +0300

    Done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,16 +2,42 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div className='relative h-[20]'>
-      <video
-        className='w-full h-[20] object-cover brightness-[60%] transition duration-500'
-        autoPlay
-        muted
-        loop
-        src='./vid.mp4'
-        style={{ objectFit: 'cover' }}
-      />
+    <div className="relative h-[36.25vw]">
+    <video  className="w-full h-[36.25vw] object-cover brightness-[60%] transition duration-500" autoPlay muted loop src='./vid.mp4'></video>
+    <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16">
+      <p className="text-white text-1xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-xl">
+      
+      </p>
+      <p className="text-white text-[8px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[50%] drop-shadow-xl">
+    
+      </p>
+      <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
+      
+        <button
+       
+          className="
+          bg-white
+          text-white
+            bg-opacity-30 
+            rounded-md 
+            py-1 md:py-2 
+            px-2 md:px-4
+            w-auto 
+            text-xs lg:text-lg 
+            font-semibold
+            flex
+            flex-row
+            items-center
+            hover:bg-opacity-20
+            transition
+          "
+          >
+          
+            More Info
+        </button>
+      </div>
     </div>
+  </div>
   );
 };
 
