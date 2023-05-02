cec9d5ff23af72de58e934a355a0df8d324aca3a
QUANDEVV <vercelploy@gmail.com>
2023-05-02 10:22:48 +0300

    Done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className='relative h-[20]'>
+      <video
+        className='w-full h-[20] object-cover brightness-[60%] transition duration-500'
+        autoPlay
+        muted
+        loop
+        src='./vid.mp4'
+        style={{ objectFit: 'cover' }}
+      />
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
