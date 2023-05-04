117f0ccbb2f4e638dba591501fc2395dd6ff3a4a
QUANDEVV <vercelploy@gmail.com>
2023-05-04 19:03:33 +0300

    updated each cards

--- src/Home/Continue.js
+++ src/Home/Continue.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
-
+import ReactPlayer from 'react-player';
 
 const Continue = () => {
   const [movies, setMovies] = useState([]);
-  const [hoverIndex, setHoverIndex] = useState(-1);
 
   useEffect(() => {
     fetch('http://localhost:8002/movies')
@@ -11,35 +10,28 @@ const Continue = () => {
       .then(data => setMovies(data));
   }, []);
 
-  const handleHover = index => {
-    setHoverIndex(index);
-  };
+  if (movies.length === 0) {
+    return <div>Loading...</div>;
+  }
 
-  const handleLeave = () => {
-    setHoverIndex(-1);
-  };
-  
   return (
-    <div className=" container mx-auto my-5 mt-10 text-black dark:text-white">
-      <h2 className="text-3xl font-bold mb-5">Continue Watching For Michael</h2>
+    <div className="container mx-auto my-5 mt-10 text-black dark:text-white">
+      <h2 className="text-3xl font-bold mb-5">Scifi</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-10 mb-20">
-        {movies.map((movie, index) => (
-          <div
-            key={movie.id}
-            className={`bg-black rounded-lg shadow-lg overflow-hidden ${
-              index === hoverIndex ? 'scale-110 transition duration-300 h-70' : 'h-70'
-            }`}
-            onMouseEnter={() => handleHover(index)}
-            onMouseLeave={handleLeave}
-            style={{ display: 'flex', flexDirection: 'column' }}
-          >
-            <img src={movie.poster} alt={movie.title} className="w-full h-48 object-cover" />
-            {index === hoverIndex && (
-              <div className="px-4 py-2">
-                <h3 className="text-lg font-bold mb-2">{movie.title}</h3>
-                <p className="text-gray-700 text-base">{movie.description}</p>
-              </div>
-            )}
+        {movies.map((movie) => (
+          <div key={movie.id} className="bg-black rounded-lg shadow-lg overflow-hidden">
+            <ReactPlayer
+              url={movie.url}
+              className="w-full h-full object-cover object-center"
+              controls
+              light={movie.poster} // use the movie poster as a preview image
+              width="100%"
+              height="100%"
+            />
+            <div className="px-4 py-2">
+              <h3 className="text-lg font-bold mb-2">{movie.title}</h3>
+              <p className="text-gray-700 text-base">{movie.description}</p>
+            </div>
           </div>
         ))}
       </div>
@@ -47,8 +39,5 @@ const Continue = () => {
   );
 };
 
-export default Continue;   
-
-
-
+export default Continue;
 
--- src/Home/Featured.js
+++ src/Home/Featured.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import ReactPlayer from 'react-player';
 
 const Featured = () => {
   const [movies, setMovies] = useState([]);
-  const [hoverIndex, setHoverIndex] = useState(-1);
 
   useEffect(() => {
     fetch('http://localhost:8002/Featured')
@@ -10,35 +10,28 @@ const Featured = () => {
       .then(data => setMovies(data));
   }, []);
 
-  const handleHover = index => {
-    setHoverIndex(index);
-  };
-
-  const handleLeave = () => {
-    setHoverIndex(-1);
-  };
+  if (movies.length === 0) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div className="container mx-auto my-5 mt-20 text-black dark:text-white">
-      <h2 className="text-3xl font-bold mb-5">Featured</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 ">
-        {movies.map((movie, index) => (
-          <div
-            key={movie.id}
-            className={`bg-black rounded-lg shadow-lg overflow-hidden ${
-              index === hoverIndex ? 'scale-110 transition duration-300 h-82' : 'h-51'
-            }`}
-            onMouseEnter={() => handleHover(index)}
-            onMouseLeave={handleLeave}
-            style={{ display: 'flex', flexDirection: 'column' }}
-          >
-            <img src={movie.poster} alt={movie.title} className="w-full h-48 object-cover" />
-            {index === hoverIndex && (
-              <div className="px-4 py-2">
-                <h3 className="text-lg font-bold mb-2">{movie.title}</h3>
-                <p className="text-gray-700 text-base">{movie.description}</p>
-              </div>
-            )}
+    <div className="container mx-auto my-5 mt-10 text-black dark:text-white">
+      <h2 className="text-3xl font-bold mb-5">Scifi</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-10 mb-20">
+        {movies.map((movie) => (
+          <div key={movie.id} className="bg-black rounded-lg shadow-lg overflow-hidden">
+            <ReactPlayer
+              url={movie.url}
+              className="w-full h-full object-cover object-center"
+              controls
+              light={movie.poster} // use the movie poster as a preview image
+              width="100%"
+              height="100%"
+            />
+            <div className="px-4 py-2">
+              <h3 className="text-lg font-bold mb-2">{movie.title}</h3>
+              <p className="text-gray-700 text-base">{movie.description}</p>
+            </div>
           </div>
         ))}
       </div>
@@ -47,3 +40,4 @@ const Featured = () => {
 };
 
 export default Featured;
+
