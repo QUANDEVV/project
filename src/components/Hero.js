85a57b3bc5ec7eebd9e5faf091e3df0c492c4b56
QUANDEVV <vercelploy@gmail.com>
2023-05-04 08:56:02 +0300

    Added hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,11 +6,11 @@ const Hero = () => {
     <video  className="w-full h-[36.25vw] object-cover brightness-[60%] transition duration-500" autoPlay muted loop src='./vid.mp4'></video>
     <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16">
       <p className="text-white text-1xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-xl">
-        Hello
+      The Adventures of Sparky the Dragon"
       
       </p>
       <p className="text-white text-[8px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[50%] drop-shadow-xl">
-        Hello
+      heartwarming animated cartoon series that follows the exciting and magical journey of Sparky, a young and mischievous dragon with a zest for life. Set in the enchanting Dragonland, the series takes viewers on a thrilling adventure filled with friendship, discovery, and life lessons.
     
       </p>
       <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
